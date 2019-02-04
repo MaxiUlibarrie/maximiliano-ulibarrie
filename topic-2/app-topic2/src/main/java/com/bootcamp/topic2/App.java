a70ccbf59fea3e272ff261a25c2d5df238bb7184
MaxiUlibarrie <agustin.ulibarrie@gmail.com>
2019-02-04 09:06:13 -0300

    created maven project

---
+++ topic-2/app-topic2/src/main/java/com/bootcamp/topic2/App.java
@@ -0,0 +1,13 @@
+package com.bootcamp.topic2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
