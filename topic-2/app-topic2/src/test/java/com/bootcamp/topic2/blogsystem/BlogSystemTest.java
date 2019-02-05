16a8051c8bc1c4f0f4ce3e7208c1e3eb00dc5197
MaxiUlibarrie <agustin.ulibarrie@gmail.com>
2019-02-05 20:38:24 -0300

    refactored BlogSystemTest, added @Before

--- topic-2/app-topic2/src/test/java/com/bootcamp/topic2/blogsystem/BlogSystemTest.java
+++ topic-2/app-topic2/src/test/java/com/bootcamp/topic2/blogsystem/BlogSystemTest.java
@@ -2,16 +2,27 @@ package com.bootcamp.topic2.blogsystem;
 
 import com.bootcamp.topic2.blogsystemdao.EntryDAO;
 import java.time.LocalDateTime;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class BlogSystemTest {
+  private EntryDAO entryDAO;
+  private Blog blog;
+
+  @Before
+  public void setup() {
+    blog = new Blog("ownerName","www.blog.com");
+
+    entryDAO = mock(EntryDAO.class);
+    when(entryDAO.recoveryById(1)).thenReturn(new Entry("title1","topic1","body1"));
+    when(entryDAO.recoveryById(2)).thenReturn(new Entry("title2","topic2","body2"));
+    when(entryDAO.recoveryById(3)).thenReturn(new Entry("title3","topic3","body3"));
+  }
+
   @Test
   public void whenPostNewEntryThenItExistsInTheBlogList() {
-    Blog blog = new Blog("ownerName","www.blog.com");
-    EntryDAO entryDAO = mock(EntryDAO.class);
-    when(entryDAO.recoveryById(1)).thenReturn(new Entry("title","topic","body"));
     Entry entry = entryDAO.recoveryById(1);
     blog.addEntry(entry);
 
@@ -20,13 +31,6 @@ public class BlogSystemTest {
 
   @Test
   public void whenDeleteExistingEntryThenItIsGoneFromTheBlog() {
-    EntryDAO entryDAO = mock(EntryDAO.class);
-    Blog blog = new Blog("ownerName","www.blog.com");
-
-    when(entryDAO.recoveryById(1)).thenReturn(new Entry("title1","topic1","body1"));
-    when(entryDAO.recoveryById(2)).thenReturn(new Entry("title2","topic2","body2"));
-    when(entryDAO.recoveryById(3)).thenReturn(new Entry("title3","topic3","body3"));
-
     Entry entry1 = entryDAO.recoveryById(1);
     blog.addEntry(entry1);
 
@@ -43,13 +47,6 @@ public class BlogSystemTest {
 
   @Test
   public void whenShowMostRecentEntryThenTheseHaveToBeInOrderDate() {
-    EntryDAO entryDAO = mock(EntryDAO.class);
-    Blog blog = new Blog("ownerName","www.blog.com");
-
-    when(entryDAO.recoveryById(1)).thenReturn(new Entry("title1","topic1","body1"));
-    when(entryDAO.recoveryById(2)).thenReturn(new Entry("title2","topic2","body2"));
-    when(entryDAO.recoveryById(3)).thenReturn(new Entry("title3","topic3","body3"));
-
     Entry entry1 = entryDAO.recoveryById(1);
     entry1.setDatetime(LocalDateTime.of(2018,2,5,16,40));
     blog.addEntry(entry1);
