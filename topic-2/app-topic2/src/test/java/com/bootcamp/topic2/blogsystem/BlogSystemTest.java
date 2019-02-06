92d6c9b6300acb086705342b72dd53574ecae083
MaxiUlibarrie <agustin.ulibarrie@gmail.com>
2019-02-06 10:55:58 -0300

    refactored BlogSystemTest - whenShowMostRecent(...) method

--- topic-2/app-topic2/src/test/java/com/bootcamp/topic2/blogsystem/BlogSystemTest.java
+++ topic-2/app-topic2/src/test/java/com/bootcamp/topic2/blogsystem/BlogSystemTest.java
@@ -19,6 +19,7 @@ public class BlogSystemTest {
     when(entryDAO.recoveryById(1)).thenReturn(new Entry("title1","topic1","body1"));
     when(entryDAO.recoveryById(2)).thenReturn(new Entry("title2","topic2","body2"));
     when(entryDAO.recoveryById(3)).thenReturn(new Entry("title3","topic3","body3"));
+    when(entryDAO.recoveryById(4)).thenReturn(new Entry("title4","topic4","body4"));
   }
 
   @Test
@@ -48,16 +49,20 @@ public class BlogSystemTest {
   @Test
   public void whenShowMostRecentEntryThenTheseHaveToBeInOrderDate() {
     Entry entry1 = entryDAO.recoveryById(1);
-    entry1.setDatetime(LocalDateTime.of(2018,2,5,16,40));
+    entry1.setDatetime(LocalDateTime.of(2019,2,5,16,40));
     blog.addEntry(entry1);
 
     Entry entry2 = entryDAO.recoveryById(2);
-    entry2.setDatetime(LocalDateTime.of(2019,2,3,16,43));
+    entry2.setDatetime(LocalDateTime.of(2019,2,5,16,43));
     blog.addEntry(entry2);
 
     Entry entry3 = entryDAO.recoveryById(3);
-    entry3.setDatetime(LocalDateTime.of(2019,2,5,10,43));
+    entry3.setDatetime(LocalDateTime.of(2018,2,5,10,43));
     blog.addEntry(entry3);
+
+    Entry entry4 = entryDAO.recoveryById(4);
+    entry4.setDatetime(LocalDateTime.of(2019,1,1,10,43));
+    blog.addEntry(entry4);
 
     boolean areInOrder = true;
     LocalDateTime previous, next;
