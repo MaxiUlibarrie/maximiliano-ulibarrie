6227099895e1d799d3f3718bbfc9f90340abba0d
MaxiUlibarrie <agustin.ulibarrie@gmail.com>
2019-02-11 11:31:32 -0300

    removed public modifiers from UserService

--- topic-3/app-topic3/src/main/java/com/bootcamp/topic3/users/UserService.java
+++ topic-3/app-topic3/src/main/java/com/bootcamp/topic3/users/UserService.java
@@ -6,39 +6,39 @@ public interface UserService {
    * Creates a new user.
    * @param user
    */
-  public void createUser(User user);
+  void createUser(User user);
 
   /**
    * Updates the user's name.
    * @param idUser
    * @param name
    */
-  public void updateUserNameById(Long idUser, String name);
+  void updateUserNameById(Long idUser, String name);
 
   /**
    * Updates the user's email.
    * @param idUser
    * @param email
    */
-  public void updateUserEmailById(Long idUser, String email);
+  void updateUserEmailById(Long idUser, String email);
 
   /**
    * Updates the user's password.
    * @param idUser
    * @param password
    */
-  public void updateUserPasswordById(Long idUser, String password);
+  void updateUserPasswordById(Long idUser, String password);
 
   /**
    * Returns user by id.
    * @param idUser
    * @return
    */
-  public User getUserById(Long idUser);
+  User getUserById(Long idUser);
 
   /**
    * Deletes user by id.
    * @param idUser
    */
-  public void deleteUserById(Long idUser);
+  void deleteUserById(Long idUser);
 }
